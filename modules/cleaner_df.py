f2a6df55140ee4b33035812ed442271499f5d549
Mainak-Mitra <mainakmitra.job@gmail.com>
2024-02-06 15:09:31 +0600

    DF cleaner removed

--- modules/cleaner_df.py
+++ modules/cleaner_df.py
@@ -1,32 +1,3 @@
-import numpy as np
-import pandas as pd
-from app_loggs import get_logger
-
-my_logger = get_logger("DfCleaner")
-my_logger.debug("Cleaning Module Loaded successfully!")
-
-
-class DfCleaner():
-    """
-        Has functions for cleans pandas data frame by removing duplicates, 
-        droping columns or rows and more.
-    """
-
-    def __init__(self):
-        pass
-
-    def fixLabel(self, label: list) -> list:
-        """convert list of labels to lowercase separated by underscore
-
-        Args:
-            label (list): list of labels 
-
-        Returns:
-            list: list of labels in lower case, separated by underscore
-        """
-        label = label.strip()
-        label = label.replace(' ', '_').replace('.', '').replace('/', '_')
-        return label.lower()
 
     def drop_duplicate(self, df: pd.DataFrame) -> pd.DataFrame:
         """drop duplicate rows
